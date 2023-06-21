5a3fec69f2a4000b4e66fd47b3d0fdb10333ebfb
Harjot Singh <124405413+Harjot-15@users.noreply.github.com>
2023-06-21 03:23:40 -0400

    Update header.php

--- PHP-Assignment-1/header.php
+++ PHP-Assignment-1/header.php
@@ -9,7 +9,7 @@
             <a href="#"><img src="https://fiverr-res.cloudinary.com/t_main1,q_auto,f_auto/gigs/267499311/original/fba5d262e9bfb87dd63cce778d0ac06aef1cb837.jpg" alt="Logo"></a>
         </div>
         <ul class="nav-links">
-            <li><button class="nav-button" onclick="location.href='https://php-assignment-1.harjotsingh1994.repl.co/'">Home</button></li>
+            <li><button class="nav-button" onclick="location.href='index.php'">Home</button></li>
             <li><button class="nav-button" onclick="location.href='About.php'">About</button></li>
             <li><button class="nav-button" onclick="scrollToSection('contact')">Contact</button></li>
         </ul>
