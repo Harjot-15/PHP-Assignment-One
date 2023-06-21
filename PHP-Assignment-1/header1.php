e5a99fc4c3e93f25cc8fd53754cbbb3c5d0e7153
Harjot Singh <124405413+Harjot-15@users.noreply.github.com>
2023-06-21 03:25:15 -0400

    Update header1.php

--- PHP-Assignment-1/header1.php
+++ PHP-Assignment-1/header1.php
@@ -2,6 +2,13 @@
 <html lang="en">
 <head>
     <script src="script.js"></script>
+    <script>
+        function scrollToSection(sectionId) {
+            document.getElementById(sectionId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    </script>
 </head>
 <body>
     <nav>
@@ -9,9 +16,9 @@
             <a href="#"><img src="https://fiverr-res.cloudinary.com/t_main1,q_auto,f_auto/gigs/267499311/original/fba5d262e9bfb87dd63cce778d0ac06aef1cb837.jpg" alt="Logo"></a>
         </div>
         <ul class="nav-links">
-            <li><button class="nav-button" onclick="location.href='https://php-assignment-1.harjotsingh1994.repl.co/'">Home</button></li>
+            <li><button class="nav-button" onclick="location.href='index.php'">Home</button></li>
             <li><button class="nav-button" onclick="location.href='About.php'">About</button></li>
-            <li><button class="nav-button" onclick="location.href='https://php-assignment-1.harjotsingh1994.repl.co?section=contact'">Contact</button></li>
+            <li><button class="nav-button" onclick="location.href='index.php?section=contact'">Contact</button></li>
         </ul>
     </nav>
 </body>
